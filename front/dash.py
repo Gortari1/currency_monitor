10e74350436b8b17dea696b8a1d685ef5a588f50
gortari1 <agx0191@gmail.com>
2025-06-22 23:30:57 +0200

    adding login screen

--- front/dash.py
+++ front/dash.py
@@ -4,7 +4,7 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("500x350")
+root.geometry("900x250")
 
 def login():
     print("Test")
@@ -12,7 +12,7 @@ def login():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
+label = customtkinter.CTkLabel(master=frame, text="Login", font=("Roboto", 24))
 label.pack(pady=12, padx=10)
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
@@ -21,10 +21,7 @@ entry1.pack(pady=12, padx=10)
 entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
 entry2.pack(pady=12, padx=10)
 
-root.mainloop()
-
-
-button = customtkinter.CTkCheckBox(master=frame, text="Login", command=login)
+button = customtkinter.CTkButton(master=frame, text="Log in", command=login)
 button.pack(pady=12, padx=10)
 
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
